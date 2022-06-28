a6f11e8d8d7e694cd514afe9005c44a92686ca93
Artic67 <sirartic67@gmail.com>
2022-06-28 18:56:02 +0300

    Added main.go

--- main.go
+++ main.go
@@ -6,11 +6,35 @@ import (
 	"log"
 	"os"
 
-	parser "github.com/Artic67/architecture-lab4/parser"
-	loop "github.com/Artic67/architecture-lab4/loop"
+	parser "github.com/Artic67/al4/parser"
+	loop "github.com/Artic67/al4/loop"
 )
+
+// File name argument input
+var examplesFileName = flag.String("f", "./src/examples.txt",
+	"Write a file name with commands ")
 
 // Main functions
 func main() {
-	
+
+	flag.Parse()
+	file, error := os.Open(*examplesFileName)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	el := new(loop.EventLoop)
+	el.Start()
+
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		cmd := parser.Parse(commandLine)
+		el.Post(cmd)
+	}
+
+	el.AwaitClose()
 }
